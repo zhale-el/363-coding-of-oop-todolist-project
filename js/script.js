21d20a9c27321ea7c6566f4f93261cac045f8792
zhaleh <zhalehelmi@gmail.com>
2023-07-15 22:29:50 +0330

    completed-check-with-console-log

--- js/script.js
+++ js/script.js
@@ -9,13 +9,39 @@ class TodoList {
   constructor(todosContainer) {
     this.todosContainer = todosContainer;
     this.todos = JSON.parse(localStorage.getItem("todos")) || [];
-    this.todoInput = document.querySelector("iput");
+    this.todoInput = document.querySelector("input");
     this.addBtn = document.querySelector("#addButton");
     this.clearBtn = document.querySelector("#clearButton");
 
     this.render();
   }
-  render() {}
+  render() {
+    console.log("تودو لیست شروع به کار کرد");
+
+    this.todosContainer.innerHTMl = "";
+    this.addTodosDom();
+    this.saveTodosIntoLocalStorage();
+    this.addBtn.addEventListener("click", () => {
+      this.addNewTodo();
+    });
+    this.clearBtn.addEventListener("click", () => {
+      this.clearTodos();
+    });
+  }
+
+  addTodosDom() {
+    console.log("تودوها ب دام اضافه شد");
+  }
+  addNewTodo(newTodoTitle) {
+    console.log("New todo title:", newTodoTitle);
+  }
+  clearTodos() {
+    console.log("تودوها پاک شدند");
+  }
+
+  saveTodosIntoLocalStorage() {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
 }
 
 new TodoList(document.querySelector("#todoList"));
