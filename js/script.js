fddfebac51ef66b60284dcdc29f735597778743b
zhaleh <zhalehelmi@gmail.com>
2023-07-15 22:15:53 +0330

    creat-classes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+class Todo {
+  constructor(title) {
+    this.title = title;
+    this.isComplete = false;
+  }
+}
 
+class TodoList {
+  constructor(todosContainer) {
+    this.todosContainer = todosContainer;
+    this.todos = JSON.parse(localStorage.getItem("todos")) || [];
+    this.todoInput = document.querySelector("iput");
+    this.addBtn = document.querySelector("#addButton");
+    this.clearBtn = document.querySelector("#clearButton");
+
+    this.render();
+  }
+  render() {}
+}
+
+new TodoList(document.querySelector("#todoList"));
